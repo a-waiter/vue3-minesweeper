bd1bae3093b41ffea5d30c27f27a79ab0c533cdf
吴寅.前端.APAAS组 <yin.wu@clickpaas.com>
2022-08-31 18:43:03 +0800

    feat: flagged about commit

--- minesweepers/src/pages/index.vue
+++ minesweepers/src/pages/index.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-interface BlockState {
-  x: number
-  y: number
-  revealed: boolean
-  mine?: boolean
-  flagged?: boolean
-  adjacentMines: number
-}
+import type { BlockState } from '~/types'
 const WIDTH = 10
 const HEIGHT = 10
 
@@ -33,7 +26,7 @@ const directions = [
   [-1, -1]
 ]
 
-const state = reactive(
+const state = ref(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH }, (_, x): BlockState => {
       return {
@@ -47,7 +40,7 @@ const state = reactive(
 )
 
 let mineGenerated = false
-const dev = true
+const dev = false
 
 function getBlockClass(block: BlockState) {
   if (block.flagged) return 'bg-gray-500/10'
@@ -57,7 +50,7 @@ function getBlockClass(block: BlockState) {
 }
 
 function generateMines(initial: BlockState) {
-  for (const row of state) {
+  for (const row of state.value) {
     for (const block of row) {
       if (Math.abs(initial.y - block.x) <= 1) continue
       if (Math.abs(initial.y - block.x) <= 1) continue
@@ -68,7 +61,7 @@ function generateMines(initial: BlockState) {
 }
 
 function updateNumbers() {
-  state.forEach((row, y) => {
+  state.value.forEach((row, y) => {
     row.forEach((block, x) => {
       if (block.mine) return
 
@@ -99,7 +92,7 @@ function getSiblings(block: BlockState) {
       if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
         return undefined
       }
-      return state[y][x]
+      return state.value[y][x]
     })
     .filter(Boolean) as BlockState[]
 }
@@ -110,10 +103,30 @@ function onClick(block: BlockState) {
     mineGenerated = true
   }
 
+  if (block.flagged) {
+    return
+  }
+
   block.revealed = true
   if (block.mine) alert('BOOOOM!')
 
   expendZero(block)
+}
+
+function onRightClick(block: BlockState) {
+  if (block.revealed) return
+  block.flagged = !block.flagged
+}
+
+watchEffect(checkGameState)
+function checkGameState() {
+  if (!mineGenerated) return
+  const blocks = state.value.flat()
+  if (blocks.every((block) => block.revealed || block.flagged)) {
+    if (blocks.some((block) => block.flagged && !block.mine))
+      alert('You cheat!')
+    else alert('You win!')
+  }
 }
 </script>
 
@@ -140,13 +153,14 @@ function onClick(block: BlockState) {
           min-h-8
           m="1px"
           border="0.5 gray-400/10"
-          @click="onClick(block)"
           :class="getBlockClass(block)"
+          @click="onClick(block)"
+          @contextmenu.prevent="onRightClick(block)"
         >
           <template v-if="block.flagged">
             <div i-mdi-flag text-red />
           </template>
-          <template v-else-if="block.revealed">
+          <template v-else-if="block.revealed || dev">
             <div v-if="block.mine" i-mdi-mine />
             <div v-else font-600>
               {{ block.adjacentMines }}
